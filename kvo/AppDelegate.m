86a63c507d6166c88471c0be6a9f449116b6d01f
ss <18363857227@163.com>
2016-11-02 19:06:55 +0800

    change root view to view controller

--- kvo/AppDelegate.m
+++ kvo/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -18,7 +18,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor=[UIColor whiteColor];
+    [self.window makeKeyWindow];
+    ViewController *rootvc = [[ViewController alloc] init];
+    self.window.rootViewController = rootvc;
     return YES;
+
 }
 
 
